1410a5fed82821181603f25336271eba9a79e383
Ian Benedict Famero <ianfamero@gmail.com>
2019-02-09 22:33:55 +0800

    updates

--- functions.php
+++ functions.php
@@ -2,16 +2,16 @@
 function files() {
   wp_enqueue_style('normalize', get_theme_file_uri('css/normalize.css'));
   wp_enqueue_style('tars', get_theme_file_uri('css/tars.css'));
+  wp_enqueue_style('animate', get_theme_file_uri('css/animate.css'));
   wp_enqueue_style('style', get_theme_file_uri('css/style.css'));
   wp_enqueue_style('wp-style', get_stylesheet_uri());
-  // wp_enqueue_style('fa-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  // wp_enqueue_style('fa', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('fa', get_theme_file_uri('extensions/font-awesome/css/font-awesome.min.css'));
 
-  // wp_register_script('jquery-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js', null, null, true);
+  // wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js', null, null, true);
   wp_register_script('jquery', get_theme_file_uri('js/jquery-3.2.1.min.js'), null, null, true);
   wp_register_script('scroll-out', get_theme_file_uri('js/scroll-out.min.js'), null, null, true);
   wp_register_script('scripts', get_theme_file_uri('js/scripts.js'), null, null, true);
-  // wp_enqueue_script('jquery-cdn');
   wp_enqueue_script('jquery');
   wp_enqueue_script('scroll-out');
   wp_enqueue_script('scripts');
@@ -35,4 +35,3 @@ function widgets_init() {
 	));
 }
 add_action('widgets_init', 'widgets_init');
-
